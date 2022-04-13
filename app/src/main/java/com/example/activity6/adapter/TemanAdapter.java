36ded1a84dc76326c9aa07396c5261a916bd75ba
Finanda Wafiq <finnandawafiq@gmail.com>
2022-04-13 21:34:12 +0700

    Activity 7 Delete and Update SQLite

--- app/src/main/java/com/example/activity6/adapter/TemanAdapter.java
+++ app/src/main/java/com/example/activity6/adapter/TemanAdapter.java
@@ -1,19 +1,27 @@
 package com.example.activity6.adapter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.activity6.MainActivity;
 import com.example.activity6.R;
+import com.example.activity6.database.DBController;
 import com.example.activity6.database.Teman;
+import com.example.activity6.database.edit_teman;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHolder> {
     private ArrayList<Teman> listData;
@@ -23,24 +31,69 @@ public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHol
         this.listData = listData;
     }
 
+    private Context control;
+
+    public TemanAdapter.TemanViewHolder onCreateTemanViewHolder(ViewGroup parent, int viewType){
+        LayoutInflater layoutInf = LayoutInflater.from(parent.getContext());
+        View v = layoutInf.inflate(R.layout.row_data_teman,parent,false);
+        control = parent.getContext();
+        return new TemanViewHolder(v);
+    }
+
     @Override
     public TemanViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInf = LayoutInflater.from(parent.getContext());
         View view = layoutInf.inflate(R.layout.row_data_teman,parent,false);
+        control = parent.getContext();
         return new TemanViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull TemanViewHolder holder, int position) {
-        String nm,tlp;
+    public void onBindViewHolder(TemanAdapter.TemanViewHolder holder, int position) {
+        String nm,tlp,id;
 
+        id = listData.get(position).getId();
         nm = listData.get(position).getNama();
         tlp = listData.get(position).getTelepon();
+        DBController db = new DBController(control);
 
         holder.namaTxt.setTextSize(20);
         holder.namaTxt.setTextColor(Color.BLUE);
         holder.namaTxt.setText(nm);
         holder.teleponTxt.setText(tlp);
+
+        holder.cardku.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(control, holder.cardku);
+                popupMenu.inflate(R.menu.menu);
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.mnEdit:
+                                Intent i = new Intent(control, edit_teman.class);
+                                i.putExtra("id",id);
+                                i.putExtra("nama",nm);
+                                i.putExtra("telepon",tlp);
+                                control.startActivity(i);
+                                break;
+
+                            case R.id.mnHapus:
+                                HashMap<String,String > values = new HashMap<>();
+                                values.put("id",id);
+                                db.DeleteData(values);
+                                Intent j = new Intent(control, MainActivity.class);
+                                control.startActivity(j);
+                                break;
+                        }
+                        return true;
+                    }
+                });
+                popupMenu.show();
+                return false;
+            }
+        });
     }
 
     @Override
@@ -55,8 +108,8 @@ public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHol
         public TemanViewHolder(View view) {
             super(view);
             cardku = (CardView) view.findViewById(R.id.kartuku);
-                    namaTxt = (TextView) view.findViewById(R.id.textNama);
-                    teleponTxt = (TextView) view.findViewById(R.id.textTelepon);
+            namaTxt = (TextView) view.findViewById(R.id.textNama);
+            teleponTxt = (TextView) view.findViewById(R.id.textTelepon);
         }
     }
 }
